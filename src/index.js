7db2d7c93428b28f5b662b3f03ef38d8be907740
Daner Alejandro Salazar Colorado <daneralejandro03@gmail.com>
2025-04-08 22:31:42 -0500

    Cors problem

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ require("dotenv").config();
 const connectionDB = require("./config/database");
 const routes = require("./routes/routes");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3005;
+const port = process.env.PORT;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // listen: 2 param puerto y funcion flecha
 app.listen(port, () => {
